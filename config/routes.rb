679cbc54c6d8082a6956b964b0fd500a9a009f11
Philipp Bruns <plath.philipp@gmail.com>
2021-08-10 16:24:06 +0200

    two

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
-  get 'pages/about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # See a list of all tasks
+  get "/tasks", to: "tasks#index", as: :tasks
+  # See just one task
+  get "/tasks/:id", to: "tasks#show", as: :task
+  # See a new form
+  get "/tasks/new", to: "tasks#new", as: :new_task
+
+  # Submit the new form
+  post "/tasks", to: "tasks#create"
+
+  # # See a form to edit
+  get "/tasks/:id/edit", to: "tasks#edit", as: :edit_task
+
+  #submit the edit form
+  patch "/tasks/:id", to: "tasks#update"
+
+  delete "/tasks/:id", to: "tasks#destroy"
 end
